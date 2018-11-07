d6c57aade6d14680939710d3522f14850d1c499a
Matteo Cominetti <matteo@cominetti.org>
2018-11-07 09:54:12 +0000

    checking for new versions against appveyor's builds

--- SpeckleDynamoFunctions/Properties/AssemblyInfo.cs
+++ SpeckleDynamoFunctions/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7d9a6b97-5773-44b1-8c04-367bff0f8599")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
