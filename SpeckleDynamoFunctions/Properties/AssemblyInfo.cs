dad4e2889661b52f97b44cead3799343631d716c
Matteo Cominetti <matteo@cominetti.org>
2019-05-07 18:34:11 +0100

    fixed newtonsoft hell, switched projs to have package.config

--- SpeckleDynamoFunctions/Properties/AssemblyInfo.cs
+++ SpeckleDynamoFunctions/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SpeckleDynamoFUnctions")]
+[assembly: AssemblyTitle("SpeckleDynamoFunctions2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SpeckleDynamoFUnctions")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("SpeckleDynamoFunctions2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,4 +20,17 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7d9a6b97-5773-44b1-8c04-367bff0f8599")]
+[assembly: Guid("1dfdf56b-7e81-4f00-96e2-94d8178220f6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
