36a1c8bd4f8d93e125f38db2f0acc17b31abcce2
Matthew Swaidan <Matthew.Swaidan@woodsbagot.com>
2019-12-20 10:58:57 -0500

    Adds two components for adding a stream to a project

---
+++ SpeckleDynamo/UserControls/ProjectAddStreamUi.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace SpeckleDynamo.UserControls
+{
+  /// <summary>
+  /// Interaction logic for ReceiverUi.xaml
+  /// </summary>
+  public partial class ProjectAddStreamUi : UserControl
+  {
+    public ProjectAddStreamUi()
+    {
+      InitializeComponent();
+    }
+
+  }
+}
---
+++ SpeckleDynamo/ViewCustomizations/ProjectAddStreamViewCustomization.cs
@@ -0,0 +1,35 @@
+﻿using Dynamo.Controls;
+using Dynamo.Wpf;
+using SpeckleDynamo.UserControls;
+using System;
+
+namespace SpeckleDynamo.ViewCustomizations
+{
+  public class ProjectAddStreamCustomization : INodeViewCustomization<ProjectAddStream>
+  {
+    private ProjectAddStream addStream;
+
+    public void CustomizeView(ProjectAddStream model, NodeView nodeView)
+    {
+      var ui = new ProjectAddStreamUi();
+      if(Version.Parse(nodeView.ViewModel.DynamoViewModel.Version).CompareTo(new Version(2, 0)) < 0)
+      {
+        model.Error("Dynamo 2.0 or greater is required to run this package");
+        return;
+      }
+
+      addStream = model;
+
+      //bindings   
+      ui.DataContext = addStream;
+      ui.Loaded += addStream.AddedToDocument;
+
+      nodeView.inputGrid.Children.Add(ui);
+      SpeckleDynamo.ProjectAddStream.viewModel = nodeView;
+    }
+
+    public void Dispose()
+    {
+    }
+  }
+}
